41cef52fc0cbbd6dd2aa4a22e82b0b4ac5d6db0d
Arman .M <Arman.M.12121@Gmail.com>
2023-05-19 03:37:38 +0330

    Patch 1

--- Minimum Rooms/ChartWindow.Designer.cs
+++ Minimum Rooms/ChartWindow.Designer.cs
@@ -37,12 +37,19 @@
             // 
             // Chart
             // 
+            this.Chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Chart.BorderlineColor = System.Drawing.Color.Aqua;
+            this.Chart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
             chartArea1.Name = "ChartArea1";
             this.Chart.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.Chart.Legends.Add(legend1);
-            this.Chart.Location = new System.Drawing.Point(102, 72);
+            this.Chart.Location = new System.Drawing.Point(110, 78);
+            this.Chart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Chart.Name = "Chart";
+            this.Chart.Padding = new System.Windows.Forms.Padding(11, 12, 11, 12);
             series1.BorderWidth = 3;
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
@@ -53,16 +60,19 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.Chart.Series.Add(series1);
-            this.Chart.Size = new System.Drawing.Size(1130, 595);
+            this.Chart.Size = new System.Drawing.Size(838, 475);
             this.Chart.TabIndex = 0;
             this.Chart.Text = "classes every hour";
             // 
             // ChartWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1346, 723);
+            this.ClientSize = new System.Drawing.Size(1078, 634);
             this.Controls.Add(this.Chart);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "ChartWindow";
             this.Text = "Chart";
             this.Load += new System.EventHandler(this.ChartWindow_Load);
